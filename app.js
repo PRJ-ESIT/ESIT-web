b3d865a6a93762fee5264d5160bcdd25c68b8429
Oleksandr Zhurbenko <xaerok.od.ua@gmail.com>
2016-09-29 15:44:23 -0700

    Webpack hot module replacement

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 var express = require('express');
-
 var app = express();
-
 var path = require('path');
 
-app.use(express.static('dist'));
+//webpack hot load
+var webpack = require('webpack');
+var webpackConfig = require('./config/webpack.config');
+var compiler = webpack(webpackConfig);
+app.use(require("webpack-dev-middleware")(compiler, {
+    noInfo: true, publicPath: webpackConfig.output.publicPath
+}));
+app.use(require("webpack-hot-middleware")(compiler));
+//end of webpack hot load
 
+app.use(express.static('dist'));
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'src/client/index.html'));
+  res.sendFile(path.join(__dirname, './src/client/index.html'));
 });
 
 app.listen(3000, function(err) {
