3e2f76f369dab32d3950e2a9db4b20d1c267eae8
Klever Loza Vega <klozavega@myseneca.ca>
2016-10-01 17:26:29 -0700

    Updated Create New Sale form

--- src/app/components/sales/NewSale.jsx
+++ src/app/components/sales/NewSale.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Tabs, Tab, TextField, Divider, RadioButton,
   RadioButtonGroup, RaisedButton, Card, CardActions,
   CardHeader, CardMedia, CardTitle, CardText, FlatButton,
-  DatePicker} from 'material-ui';
+  DatePicker, TimePicker, Toggle, Checkbox} from 'material-ui';
 
 const styles = {
 headline: {
@@ -19,7 +19,26 @@ export default class NewSale extends React.Component {
     super(props);
     this.state = {
       value: 'a',
-      email: "",
+      saleNumber: "",
+      fname: '',
+      lname: '',
+      address: '',
+      unit: '',
+      city: '',
+      prov: '',
+      postalCode: '',
+      enbridgeNum: '',
+      email: '',
+      homePhone: '',
+      cellPhone: '',
+      installationDate: '',
+      installationTime: '',
+      notes: '',
+      salesRepId: '',
+      applicationNumber: '',
+      homeownerSignature:'',
+      programType:'',
+      salesRepSignature:'',
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -30,12 +49,11 @@ export default class NewSale extends React.Component {
     });
   };
 
-  handleTextFieldChange(id, value){
-    console.log(id, value);
-    this.setState({
-      value: value,
-      //this.state[id]: value,
-    });
+  handleTextChange(fieldname, event) {
+    event.stopPropagation();
+    var obj = {};
+    obj[fieldname] = event.target.value;
+    this.setState(obj);
   };
 
   render() {
@@ -48,74 +66,120 @@ export default class NewSale extends React.Component {
         <Tab label="Rental Agreement Form" value="a" >
           <div>
             <TextField
-              hintText="Sale Number"
-              //type = "saleNumber"
-              //onChange = {this.handleTextFieldChange.bind(this, "saleNumber", value)}
+              hintText="123-4567"
+              errorText="This field is required"
+              floatingLabelText="Sale Number"
+              value={this.state.saleNumber}
+              onChange={this.handleTextChange.bind(this, "saleNumber")}
             />
             <br />
             <br />
             <Divider />
             <br />
             <TextField
-              hintText="First Name"
-              //type = "email"
-              //onChange = {this.handleTextFieldChange.bind(this, "email", value)}
+              hintText="John"
+              errorText="This field is required"
+              floatingLabelText="First Name"
+              value={this.state.fname}
+              onChange={this.handleTextChange.bind(this, "fname")}
             />
             &nbsp;
             &nbsp;
             &nbsp;
             <TextField
-              hintText="Last Name"
-            />
-            <br />
-            <TextField
-              hintText="Address"
-            />
-            &nbsp;
-            &nbsp;
-            &nbsp;
-            <TextField
-              hintText="Unit #"
+              hintText="Doe"
+              errorText="This field is required"
+              floatingLabelText="Last Name"
+              value={this.state.lname}
+              onChange={this.handleTextChange.bind(this, "lname")}
             />
             <br />
             <TextField
-              hintText="City"
+              hintText="123 Fake Street"
+              errorText="This field is required"
+              floatingLabelText="Address"
+              value={this.state.address}
+              onChange={this.handleTextChange.bind(this, "address")}
             />
             &nbsp;
             &nbsp;
             &nbsp;
             <TextField
-              hintText="Province"
-            />
-            <br />
-            <TextField
-              hintText="Postal Code"
-            />
-            &nbsp;
-            &nbsp;
-            &nbsp;
-            <TextField
-              hintText="Enbridge #"
+              hintText="77"
+              errorText="This field is required"
+              floatingLabelText="Unit #"
+              value={this.state.unit}
+              onChange={this.handleTextChange.bind(this, "unit")}
             />
             <br />
             <TextField
-              hintText="Home Phone"
+              hintText="Toronto"
+              errorText="This field is required"
+              floatingLabelText="City"
+              value={this.state.city}
+              onChange={this.handleTextChange.bind(this, "city")}
             />
             &nbsp;
             &nbsp;
             &nbsp;
             <TextField
-              hintText="Cell Phone"
+              hintText="Ontario"
+              errorText="This field is required"
+              floatingLabelText="Province"
+              value={this.state.provinces}
+              onChange={this.handleTextChange.bind(this, "provinces")}
             />
             <br />
             <TextField
-              hintText="Email"
+              hintText="M4B 5V9"
+              errorText="This field is required"
+              floatingLabelText="Postal Code"
+              value={this.state.postalCode}
+              onChange={this.handleTextChange.bind(this, "postalCode")}
+            />
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            <TextField
+              hintText="1234567890"
+              errorText="This field is required"
+              floatingLabelText="Enbridge Gas Number"
+              value={this.state.enbridgeNum}
+              onChange={this.handleTextChange.bind(this, "enbridgeNum")}
+            />
+            <br />
+            <TextField
+              hintText="(416) 123-4567"
+              errorText="This field is required"
+              floatingLabelText="Home Phone"
+              value={this.state.homePhone}
+              onChange={this.handleTextChange.bind(this, "homePhone")}
+            />
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            <TextField
+              hintText="(416) 123-4567"
+              errorText="This field is required"
+              floatingLabelText="Cell Phone"
+              value={this.state.cellPhone}
+              onChange={this.handleTextChange.bind(this, "cellPhone")}
+            />
+            <br />
+            <TextField
+              hintText="name@domain.com"
+              errorText="This field is required"
+              floatingLabelText="Email"
+              value={this.state.email}
+              onChange={this.handleTextChange.bind(this, "email")}
             />
             <br />
             <br />
             <Divider />
             <br />
-            <RadioButtonGroup name="programType">
+            <RadioButtonGroup name="programType"
+              valueSelected={this.state.programType}
+              onChange={this.handleTextChange.bind(this, "programType")}>
               <RadioButton
                 value="1"
                 label="Whole Home Filter"
@@ -137,43 +201,69 @@ export default class NewSale extends React.Component {
             <Divider />
             <br />
             <DatePicker
-              hintText="Installation Date" container="inline"
+              hintText="2010-08-20" container="inline"
+              errorText="This field is required"
+              floatingLabelText="Installation Date"
+              // value={this.state.installationDate}
+              // onChange={this.handleTextChange.bind(this, "installationDate")}
             />
-            &nbsp;
-            &nbsp;
-            &nbsp;
-            <TextField
+            <TimePicker
               hintText="Installation Time"
+              errorText="This field is required"
+              floatingLabelText="Installation Time"
+              // value={this.state.installationTime}
             />
             <br />
             <TextField
               hintText="Notes"
               multiLine={true}
               rows={5}
+              // errorText="This field is required"
+              floatingLabelText="Notes"
+              value={this.state.notes}
+              onChange={this.handleTextChange.bind(this, "notes")}
             />
             <br />
             <br />
             <Divider />
             <br />
             <br />
-            <TextField
-              hintText="Homeowner's Signature"
+            <Checkbox
+              label="Homeowner Signature"
+              style={styles.checkbox}
+              labelPosition="left"
+              checked={true}
+              disabled={true}
+              value={this.state.homeownerSignature}
+              onCheck = {this.handleTextChange.bind(this, "homeownerSignature")}
             />
             &nbsp;
             &nbsp;
             &nbsp;
-            <TextField
-              hintText="Date"
+            <DatePicker
+              hintText="2010-08-20"
+              container="inline"
+              errorText="This field is required"
+              floatingLabelText="Signature Date"
+              // value={this.state.installationDate}
+              // onChange={this.handleTextChange.bind(this, "installationDate")}
             />
             <br />
-            <TextField
-              hintText="Sales Rep Signature"
+            <Checkbox
+              label="Sales Rep Signature"
+              style={styles.checkbox}
+              labelPosition="left"
+              checked={true}
+              disabled={true}
+              value={this.state.salesRepSignature}
+              onCheck = {this.handleTextChange.bind(this, "salesRepSignature")}
             />
-            &nbsp;
-            &nbsp;
-            &nbsp;
             <TextField
-              hintText="Sales Rep ID"
+              hintText="123-4567"
+              errorText="This field is required"
+              floatingLabelText="Sales Rep ID"
+              value={this.state.salesRepId}
+              onChange={this.handleTextChange.bind(this, "salesRepId")}
             />
             <br />
             <br />
@@ -195,74 +285,130 @@ export default class NewSale extends React.Component {
           <div>
             <br />
             <TextField
-              hintText="Sale Number"
+              hintText="123-4567"
+              errorText="This field is required"
+              floatingLabelText="Sale Number"
+              value={this.state.saleNumber}
+              onChange={this.handleTextChange.bind(this, "saleNumber")}
             />
             &nbsp;
             &nbsp;
             &nbsp;
             <TextField
-              hintText="Application Number"
+              hintText="123-4567"
+              errorText="This field is required"
+              floatingLabelText="Application Number"
+              value={this.state.applicationNumber}
+              onChange={this.handleTextChange.bind(this, "applicationNumber")}
             />
             <br />
-            <TextField
-              hintText="Date"
+            <DatePicker
+              hintText="2010-08-20" container="inline"
+              errorText="This field is required"
+              floatingLabelText="Date"
+              // value={this.state.installationDate}
+              // onChange={this.handleTextChange.bind(this, "installationDate")}
             />
             <br />
             <br />
             <Divider />
             <br />
             <TextField
-              hintText="First Name"
+              hintText="John"
+              errorText="This field is required"
+              floatingLabelText="First Name"
+              value={this.state.fname}
+              onChange={this.handleTextChange.bind(this, "fname")}
             />
             &nbsp;
             &nbsp;
             &nbsp;
             <TextField
-              hintText="Last Name"
-            />
-            <br />
-            <TextField
-              hintText="Address"
-            />
-            &nbsp;
-            &nbsp;
-            &nbsp;
-            <TextField
-              hintText="Unit #"
+              hintText="Doe"
+              errorText="This field is required"
+              floatingLabelText="Last Name"
+              value={this.state.lname}
+              onChange={this.handleTextChange.bind(this, "lname")}
             />
             <br />
             <TextField
-              hintText="City"
+              hintText="123 Fake Street"
+              errorText="This field is required"
+              floatingLabelText="Address"
+              value={this.state.address}
+              onChange={this.handleTextChange.bind(this, "address")}
             />
             &nbsp;
             &nbsp;
             &nbsp;
             <TextField
-              hintText="Province"
-            />
-            <br />
-            <TextField
-              hintText="Postal Code"
-            />
-            &nbsp;
-            &nbsp;
-            &nbsp;
-            <TextField
-              hintText="Enbridge #"
+              hintText="77"
+              errorText="This field is required"
+              floatingLabelText="Unit #"
+              value={this.state.unit}
+              onChange={this.handleTextChange.bind(this, "unit")}
             />
             <br />
             <TextField
-              hintText="Home Phone"
+              hintText="Toronto"
+              errorText="This field is required"
+              floatingLabelText="City"
+              value={this.state.city}
+              onChange={this.handleTextChange.bind(this, "city")}
             />
             &nbsp;
             &nbsp;
             &nbsp;
             <TextField
-              hintText="Cell Phone"
+              hintText="Ontario"
+              errorText="This field is required"
+              floatingLabelText="Province"
+              value={this.state.provinces}
+              onChange={this.handleTextChange.bind(this, "provinces")}
             />
             <br />
             <TextField
-              hintText="Email"
+              hintText="M4B 5V9"
+              errorText="This field is required"
+              floatingLabelText="Postal Code"
+              value={this.state.postalCode}
+              onChange={this.handleTextChange.bind(this, "postalCode")}
+            />
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            <TextField
+              hintText="1234567890"
+              errorText="This field is required"
+              floatingLabelText="Enbridge Gas Number"
+              value={this.state.enbridgeNum}
+              onChange={this.handleTextChange.bind(this, "enbridgeNum")}
+            />
+            <br />
+            <TextField
+              hintText="(416) 123-4567"
+              errorText="This field is required"
+              floatingLabelText="Home Phone"
+              value={this.state.homePhone}
+              onChange={this.handleTextChange.bind(this, "homePhone")}
+            />
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            <TextField
+              hintText="(416) 123-4567"
+              errorText="This field is required"
+              floatingLabelText="Cell Phone"
+              value={this.state.cellPhone}
+              onChange={this.handleTextChange.bind(this, "cellPhone")}
+            />
+            <br />
+            <TextField
+              hintText="name@domain.com"
+              errorText="This field is required"
+              floatingLabelText="Email"
+              value={this.state.email}
+              onChange={this.handleTextChange.bind(this, "email")}
             />
             <br />
             <br />
@@ -295,14 +441,24 @@ export default class NewSale extends React.Component {
             <br />
             <Divider />
             <br />
-            <TextField
-              hintText="Homeowner Signature"
+            <Checkbox
+              label="Homeowner Signature"
+              style={styles.checkbox}
+              labelPosition="left"
+              checked={true}
+              disabled={true}
+              value={this.state.homeownerSignature}
+              onCheck = {this.handleTextChange.bind(this, "homeownerSignature")}
             />
             &nbsp;
             &nbsp;
             &nbsp;
-            <TextField
-              hintText="Date"
+            <DatePicker
+              hintText="2010-08-20" container="inline"
+              errorText="This field is required"
+              floatingLabelText="Installation Date"
+              // value={this.state.installationDate}
+              // onChange={this.handleTextChange.bind(this, "installationDate")}
             />
             <br />
             <br />
