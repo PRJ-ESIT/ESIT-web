dbafb6ec78a694ba30e500c42f6def5d2ca59916
Oleksandr Zhurbenko <xaerok.od.ua@gmail.com>
2017-01-10 22:44:16 -0800

    Added authentication

--- src/app/components/AppContainer.jsx
+++ src/app/components/AppContainer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import App from './App.jsx';
+import LoginScreen from './LoginScreen.jsx';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
@@ -8,12 +9,66 @@ export default class AppContainer extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      loggedIn: false,
+      role: undefined,
+      userId: undefined,
+      userName: undefined,
+    };
+    this.setCredentials = this.setCredentials.bind(this);
+    this.clearCredentials = this.clearCredentials.bind(this);
+  }
+
+  componentDidMount() {
+    var role = window.sessionStorage.getItem("role"); //Get item
+    var userId = window.sessionStorage.getItem("userId"); //Get item
+
+    if(role != undefined && userId != undefined) {
+      this.setState({
+        loggedIn: true,
+      });
+    }
+  }
+
+  componentWillUpdate() {
+    if(this.state.loggedIn == false) {
+      var role = window.sessionStorage.getItem("role"); //Get item
+      var userId = window.sessionStorage.getItem("userId"); //Get item
+      if(role != undefined && userId != undefined) {
+        this.setState({
+          loggedIn: true,
+        });
+      }
+    }
+  }
+
+  setCredentials(userId, firstName, lastName, role) {
+    window.sessionStorage.setItem("role", role); //Set item
+    window.sessionStorage.setItem("userId", userId); //Set item
+    this.setState({
+      userId: userId,
+      userName: firstName + lastName,
+      role: role,
+    });
+  }
+
+  clearCredentials() {
+    window.sessionStorage.clear(); //Clear storage
+    this.setState({
+      loggedIn: false,
+      userId: undefined,
+      userName: undefined,
+      role: undefined,
+    });
   }
 
   render() {
-    return (
-      <App />
-    );
+      if(this.state.loggedIn) {
+        return <App logout={this.clearCredentials} role={this.state.role}/>;
+      } else {
+        return <LoginScreen setCredentials={this.setCredentials}/>;
+      }
   }
 }
 
