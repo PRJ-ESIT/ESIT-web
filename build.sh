f5c058b4afb65b68566f2e680e6c71d07faf4c03
Oleksandr Zhurbenko <xaerok.od.ua@gmail.com>
2017-02-17 14:08:39 -0800

    Updated start.sh for both production and iOS; removed build.sh script

--- build.sh
+++
@@ -1,2 +0,0 @@
-./node_modules/.bin/webpack --config config/webpack.config.js
-cp ./dist/bundle.js ./ios/www/js/bundle.js
--- start.sh
+++ start.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+#This script catches 3 different options: production, development, ios.
+#It defaults to development if there is no option passed.
 
-ENVIRONMENT_TYPE=development
 
-NODE_ENV=$ENVIRONMENT_TYPE ./node_modules/.bin/nodemon app.js
+if [ "$1" == "ios" ]; then
+  ./node_modules/.bin/webpack --config config/webpack.production.config.js
+  cp ./dist/bundle.js ./ios/www/js/bundle.js
+else
+  if [ "$1" == "production" ]; then
+    ENVIRONMENT_TYPE=production
+    ./node_modules/.bin/webpack --config config/webpack.production.config.js
+    NODE_ENV=$ENVIRONMENT_TYPE ./node_modules/.bin/nodemon app.js
+  else
+    ENVIRONMENT_TYPE=development
+    NODE_ENV=$ENVIRONMENT_TYPE ./node_modules/.bin/nodemon app.js
+  fi
+fi
